78649ad3bdd5381d0af4799e35855aa955f212fa
ILINE2006 <teplotom71@gmail.com>
2026-03-11 15:25:51 +0700

    Добавлена обработка ошибок в запросе авторизации

--- my-app/src/utils/api.js
+++ my-app/src/utils/api.js
@@ -2,15 +2,21 @@ const API = process.env.VUE_APP_API;
 
 export const loginRequest = (user) => {
   return new Promise((resolve, reject) => {
-    fetch(`${API}/login`, {
+    fetch(`${process.env.VUE_APP_API}/login`, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json;charset=utf-8',
       },
       body: JSON.stringify(user),
     })
-      .then((response) => response.json())
-      .then((result) => resolve(result.data.user_token))
+      .then(async (response) => {
+        const data = await response.json();
+        if (!response.ok) {
+           reject(data); 
+        } else {
+           resolve(data.data.user_token);
+        }
+      })
       .catch((error) => {
         reject(error);
       });
