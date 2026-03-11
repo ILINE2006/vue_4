848a5627ba546f332585a4a367850175b1126ff2
ILINE2006 <teplotom71@gmail.com>
2026-03-11 11:26:15 +0700

    Реализована форма входа Login

--- my-app/src/router/index.js
+++ my-app/src/router/index.js
@@ -1,17 +1,46 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import store from '../store/index.js' // Подключаем хранилище
 
+// 1. СНАЧАЛА создаем охранников (функции проверок) [cite: 686-696]
+const ifNotAuthenticated = (to, from, next) => {
+  if (!store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/');
+};
+
+const ifAuthenticated = (to, from, next) => {
+  if (store.getters.isAuthenticated) {
+    next();
+    return;
+  }
+  next('/login');
+};
+
+// 2. ТОЛЬКО ПОТОМ используем их в списке страниц [cite: 699-714]
 const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: function(){
+      return import('../views/HomeView.vue');
+    }
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: function(){
+      return import('../components/Login.vue');
+    },
+    beforeEnter: ifNotAuthenticated, // Проверяем: если не авторизован - пускаем
   }
-]
+];
 
+// 3. И в конце собираем всё вместе
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+export default router;
--- my-app/src/store/index.js
+++ my-app/src/store/index.js
@@ -1,14 +1,38 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import { loginRequest } from '@/utils/api.js';
+
 
 export default createStore({
   state: {
+    token: localStorage.getItem('myAppToken') || '',
   },
   getters: {
-  },
-  mutations: {
+    isAuthenticated: (state) => !!state.token,
   },
   actions: {
+    AUTH_REQUEST: ({ commit }, user) => {
+      return new Promise((resolve, reject) => {
+        loginRequest(user)
+          .then((token) => {
+            commit('AUTH_SUCCESS', token);
+            localStorage.setItem('myAppToken', token);
+            resolve();
+          })
+          .catch(() => {
+            commit('AUTH_ERROR');
+            localStorage.removeItem('myAppToken');
+            reject();
+          });
+      });
+    },
   },
-  modules: {
-  }
-})
+  mutations: {
+    AUTH_SUCCESS: (state, token) => {
+      state.token = token;
+    },
+    AUTH_ERROR: (state) => {
+      state.token = '';
+    },
+  },
+});
+
